2ddce67c301a2d6f5aaaabfb2294d4a18fc9fb3f
yashgupta749 <guptayash9808@gmail.com>
2024-07-27 02:43:32 +0530

    deployment

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,16 @@ import { motion } from "framer-motion";
 function App() {
   return (
     <div className="app">
-      <motion.h1
-        initial={{ y: -200 }}
-        animate={{ y: 0 }}
-        transition={{ type: "spring", duration: 0.5 }}
-        whileHover={{ scale: 1.1 }}
-      >
-        Todo App
-      </motion.h1>
+      <center>
+        <motion.h1
+          initial={{ y: -200 }}
+          animate={{ y: 0 }}
+          transition={{ type: "spring", duration: 0.5 }}
+          whileHover={{ scale: 1.1 }}
+        >
+          Todo App
+        </motion.h1>
+      </center>
       <motion.div
         initial={{ y: 1000 }}
         animate={{ y: 0 }}
